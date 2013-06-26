9d7d3e35f74446350754c5a7350a0b0dff619c61
Xhacker Liu <liu.dongyuan@gmail.com>
2013-06-27 03:02:58 +0800

    Use date picker as input view.

--- ZheNar/ZNNewEventViewController.m
+++ ZheNar/ZNNewEventViewController.m
@@ -10,6 +10,11 @@
 
 @interface ZNNewEventViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *startTimeField;
+@property (weak, nonatomic) IBOutlet UITextField *endTimeField;
+@property (strong, nonatomic) UIDatePicker *startDatePicker;
+@property (strong, nonatomic) UIDatePicker *endDatePicker;
+
 @end
 
 @implementation ZNNewEventViewController
@@ -17,10 +22,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.startDatePicker = [[UIDatePicker alloc] init];
+    self.startTimeField.inputView = self.startDatePicker;
+    self.startDatePicker.minuteInterval = 15;
+    [self.startDatePicker addTarget:self action:@selector(dateChanged) forControlEvents:UIControlEventValueChanged];
+    
+    self.endDatePicker = [[UIDatePicker alloc] init];
+    self.endTimeField.inputView = self.endDatePicker;
+    self.endDatePicker.minuteInterval = 15;
+    [self.endDatePicker addTarget:self action:@selector(dateChanged) forControlEvents:UIControlEventValueChanged];
 }
 
 - (IBAction)cancelClicked:(UIBarButtonItem *)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)dateChanged
+{
+    if ([self.startTimeField isFirstResponder]) {
+        self.startTimeField.text = [NSDateFormatter localizedStringFromDate:self.startDatePicker.date dateStyle:NSDateFormatterMediumStyle timeStyle:NSDateFormatterShortStyle];
+
+    }
+    else if ([self.endTimeField isFirstResponder]) {
+        self.endTimeField.text = [NSDateFormatter localizedStringFromDate:self.endDatePicker.date dateStyle:NSDateFormatterMediumStyle timeStyle:NSDateFormatterShortStyle];
+    }
 }
 
 @end
