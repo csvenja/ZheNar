fe26c9795a9d1f6969543210d7811560dfd17ee7
csvenja <c.svenjax@gmail.com>
2013-06-26 20:10:43 +0800

    add user info view controller

---
+++ ZheNar/ZNUserInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZNUserInfoViewController.h
+//  ZheNar
+//
+//  Created by C.Svenja on 2013-06-26.
+//  Copyright (c) 2013 Zhenar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZNUserInfoViewController : UITableViewController
+
+@end
